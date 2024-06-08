9c19cc5baa6915e39041eafee5977db178febb0c
Sara Gylden <saragbusch@live.dk>
2024-06-08 23:17:43 +0200

    Revert "Update .eslintrc.js"
    This reverts commit 68fb6da9aa16c084613f9642f06857c1f9f83a1b.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
     node: true,
-    'cypress/globals': true, // Add this line
+    es2020: true,
   },
   extends: [
     'eslint:recommended',
-    'plugin:cypress/recommended', // Add this line if you're using Cypress plugin for ESLint
+    'plugin:vue/vue3-recommended',
+    'plugin:cypress/recommended', // Add Cypress recommended rules
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   plugins: [
-    'cypress', // Add this line if you're using Cypress plugin for ESLint
+    'cypress', // Add Cypress plugin
+  ],
+  rules: {
+    // Add your custom rules here
+  },
+  overrides: [
+    {
+      files: ['cypress/**/*.js'], // Apply Cypress-specific rules to test files
+      env: {
+        'cypress/globals': true, // Add Cypress globals
+      },
+    },
   ],
 };
